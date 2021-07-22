d3c6249ac108ec4669b0b223da7c7f29ad3f0de1
aniketr269 <48997459+aniketr269@users.noreply.github.com>
2021-07-22 14:37:43 +0530

    Update Week2_ExerciseSystem.java

--- Week2_ExerciseSystem.java
+++ Week2_ExerciseSystem.java
@@ -2,22 +2,22 @@ import java.util.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
-public class Exercise {
+public class ExerciseS 
+{
 	Scanner sc=new Scanner(System.in);
 	String name;
 	float duration;
 	String date;
 	float totalCalories;
-	Exercise()
+	HashMap<String,Float> typeE=new HashMap<String,Float>();
+	ExerciseS()
 	{
 		name=null;
 		duration=0.0f;
 		date=null;
 		totalCalories=0.0f;
-		
 	}
-	void input()
+void input()
 	{
 		System.out.println("Enter name of a user");
 		name=sc.nextLine();
@@ -38,154 +38,121 @@ public class Exercise {
 		DateTimeFormatter fd=DateTimeFormatter.ofPattern("dd-MM-yyyy");
 		date=t1.format(fd);
 		}
-		
 	}
-	void printData()
+	static void printDataAggregate(ExerciseS o)
 	{
-		System.out.println("Name:"+name);
-		System.out.println("Date:"+date);
-		System.out.println("Calories Burned=:"+totalCalories);
+		System.out.println("Name:"+o.name);
+		System.out.println("Calories Burned=:"+o.totalCalories);
 	}
-	
-	public static void main(String []args)
+	static void printDataDetailed(ExerciseS ob)
+	{
+		System.out.println("Name:"+ob.name);
+		System.out.println("Date:"+ob.date);
+		if(ob.typeE.containsKey("Walking"))
+		System.out.println("Calories burned in Walking:"+ob.typeE.get("Walking"));
+		if(ob.typeE.containsKey("Running"))
+		System.out.println("Calories burned in Running:"+ob.typeE.get("Running"));
+		if(ob.typeE.containsKey("Cycling"))
+		System.out.println("Calories burned in Cycling:"+ob.typeE.get("Cycling"));
+		System.out.println("Total Calories Burned=:"+ob.totalCalories);
+	}
+	float calorieWalking()
+	{
+	 float cal;
+	System.out.println("Enter duration of Walking");
+	duration=sc.nextFloat();
+	cal=((1000*duration)/120);
+	return cal;
+	}
+	float calorieRunning()
+	{
+		 float cal;
+			System.out.println("Enter duration of Running");
+			duration=sc.nextFloat();
+			cal=((1400*duration)/120);
+			return cal;
+	}
+	float calorieCycling()
+	{
+		 	float cal;
+			System.out.println("Enter duration of Cycling");
+			duration=sc.nextFloat();
+			cal=((1200*duration)/120);
+			return cal;
+	}
+	public static void main(String args[])
 	{
 		boolean ch=true;
 		Scanner sc=new Scanner(System.in);
-		ArrayList<Exercise> arr=new ArrayList<Exercise>();
+		ArrayList<ExerciseS> arr=new ArrayList<ExerciseS>();
 		int p=0;
-
-while(p!=2) 
-{
-	Exercise obj=new Exercise();
-	Walking w=new Walking();
-	Cycling c=new Cycling();
-	Running g=new Running();
-	obj.input();
-	float cal=0.0f;
-	ArrayList<Integer> a=new ArrayList<Integer>(4);
-	
-	
-	do
-	{
-		
-		System.out.println("Enter the number in which exercise is associated\n");
-		
-		
-		if(a.contains(1))
+		while(p!=2) 
 		{
-			System.out.println("Enter type of a exercise \n2:Running\n3:Cycling\n");
-		}
-		else if(a.contains(2))
+			ExerciseS obj=new ExerciseS();
+			obj.input();
+			float cal=0.0f;
+			ArrayList<Integer> a=new ArrayList<Integer>(4);
+			do
+			{
+				System.out.println("Enter the number in which exercise is associated\n");
+				if(a.contains(1))
+				{
+					System.out.println("Enter type of a exercise \n2:Running\n3:Cycling\n");
+				}
+				else if(a.contains(2))
+				{
+					System.out.println("Enter type of a exercise\n1:Walking\n3:Cycling\n");
+				}
+				else if(a.contains(3))
+				{
+					System.out.println("Enter type of a exercise\n1:Walking\n2:Running\n");
+					
+				}
+				else
+					System.out.println("Enter type of a exercise\n1:Walking\n2:Running\n3:Cycling\n");
+				int c1=sc.nextInt();
+				a.add(c1);
+				switch(c1) 
+				{
+				case 1:
+					float w=obj.calorieWalking();
+					if(w!=0)
+					obj.typeE.put("Walking",w);
+					cal=cal+w; 
+					break;
+				case 2:
+					float r=obj.calorieRunning();
+					if(r!=0)
+					obj.typeE.put("Running",r);
+					cal=cal+r;
+					break;
+				case 3:
+					float C=obj.calorieCycling();
+					if(C!=0)
+					obj.typeE.put("Cycling",C);
+					cal=cal+C;
+					break;
+				}
+			System.out.println("Does user did more exercise,if yes press 1 else press 0 to exit\n");
+			int cc=sc.nextInt();
+			if(cc==1)
+				ch=true;
+			else
+				ch=false;
+			}while(ch);
+			obj.totalCalories=cal;
+			arr.add(obj);
+			p=p+1;
+			a.clear();
+			}
+		System.out.println("How you want to display data:\n1:Aggregate Mode\n2:Detailed Mode");
+		int mode=sc.nextInt();
+		for(ExerciseS E:arr)
 		{
-			System.out.println("Enter type of a exercise\n1:Walking\n3:Cycling\n");
+			if(mode==1)
+				ExerciseS.printDataAggregate(E);
+			else if(mode==2)
+				ExerciseS.printDataDetailed(E);
 		}
-		else if(a.contains(3))
-		{
-			System.out.println("Enter type of a exercise\n1:Walking\n2:Running\n");
-			
-		}
-		else
-			System.out.println("Enter type of a exercise\n1:Walking\n2:Running\n3:Cycling\n");
-		int c1=sc.nextInt();
-		a.add(c1);
-		switch(c1) 
-		{
-		case 1:
-			w.getData();
-			cal=cal+w.calculateCalories();
-
-			break;
-		
-		case 2:
-			g.getData();
-			cal=cal+g.calculateCalories();
-			
-
-			break;
-		case 3:
-			c.getData();
-			cal=cal+c.calculateCalories();
-
-			break;
-		}
-	System.out.println("Does user did more exercise,if yes press 1 else press 0 to exit\n");
-	int cc=sc.nextInt();
-	if(cc==1)
-		ch=true;
-	else
-		ch=false;
-	}while(ch);
-	obj.totalCalories=cal;
-	arr.add(obj);
-	p=p+1;
-	a.clear();
-	
-}
-for(Exercise e:arr)
-{
-	System.out.println(e.name);
-	System.out.println(e.date);
-	System.out.println("Calories Burned"+e.totalCalories);
-}
-}
-	}
-class Walking extends Exercise
-{
-	
-	float calories;
-	Walking()
-	{
-		
-		calories=0.0f;
-	}
-	void getData()
-	{
-		System.out.println("Enter duration of Walking");
-		duration=sc.nextFloat();
-	}
-	float calculateCalories()
-	{
-		calories=((1000*duration)/120);
-		return calories;
-	}
-}
-class Running extends Exercise
-{
-	
-	float calories;
-	Running()
-	{
-		
-		calories=0.0f;
-	}
-	void getData()
-	{
-		System.out.println("Enter duration of Running");
-		duration=sc.nextFloat();
-	}
-	float calculateCalories()
-	{
-		calories=((1400*duration)/120);
-		return calories;
-	}
-}
-class Cycling extends Exercise
-{
-	
-	float calories;
-	Cycling()
-	{
-		
-		calories=0.0f;
-	}
-	void getData()
-	{
-		System.out.println("Enter duration of Cycling");
-		duration=sc.nextFloat();
-	}
-	float calculateCalories()
-	{
-		calories=((1200*duration)/120);
-		return calories;
 	}
 }
