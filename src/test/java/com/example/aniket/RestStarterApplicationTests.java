8ea86fdd27cc34faa472dd300b683143003445b4
Aniket Raj <aniket.raj@vignet.com>
2021-08-09 13:02:48 +0530

    initial commit

---
+++ src/test/java/com/example/aniket/RestStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aniket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
